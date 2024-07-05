029791e5c11c00ff141e562eace34fc2abfacf90
Matt Wilkinson <matwilmail@gmail.com>
2024-07-05 11:14:59 +0100

    set basePath for prod

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV == 'production'
+
+console.log('ODE_ENV', process.env.NODE_ENV);
+
 const nextConfig = {
   output: 'export',
+  basePath: isProd ? '/mysite' : '',
  
   // Optional: Change the output directory `out` -> `dist`
   distDir: 'dist',
