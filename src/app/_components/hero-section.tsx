f58c5864332bd4376f2892c2f508671163806d08
Matt Wilkinson <matwilmail@gmail.com>
2025-01-05 20:14:43 +0000

    temp path alteration

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -66,7 +66,7 @@ const HeroSection = () => {
         >
           <div className="rounded-lg bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src={pathPrefix() + "/assets/hero-image.jpg"}
+              src={pathPrefix() + "xxx/assets/hero-image.jpg"}
               alt="hero image"
               className="absolute rounded-lg transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={390}
